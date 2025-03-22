b6514b4e521f1982e53dc22f463b005c6ffeca64
Your Name <you@example.com>
2025-03-22 10:40:53 +0100

    Creacion LoginActivity y cambio main

--- app/src/main/java/com/plan/macap/MainActivity.kt
+++ app/src/main/java/com/plan/macap/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.plan.macap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,36 +13,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plan.macap.ui.theme.MacapTheme
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MacapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.main_activity)
+
+        // Obtener una referencia a la base de datos
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        // Escribir un mensaje en la base de datos
+        myRef.setValue("Hello, Firebase!")
+
+        // Leer el mensaje desde la base de datos
+        myRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val value = task.result?.value
+                Log.d("FirebaseTest", "Value is: $value")
+            } else {
+                Log.e("FirebaseTest", "Error getting data", task.exception)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MacapTheme {
-        Greeting("Android")
     }
 }
